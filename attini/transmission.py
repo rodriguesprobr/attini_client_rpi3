71e243dac87731cabf68599b149c7ced90e798d9
rodriguesprobr <fernando@rodrigues.pro.br>
2018-11-17 14:39:26 -0200

    Changing API capabilities

--- attini/transmission.py
+++ attini/transmission.py
@@ -32,7 +32,7 @@ def send(air_humidity, air_temperature, soil_moisture, photo_bin):
         if response.status_code == requests.codes.ok:
             util.log("Data sent.")
             try:
-                rcvd_data = response.text
+                rcvd_data = '{' + response.text.split('{',1)[0]
                 util.log("Received data: {0}".format(str(rcvd_data)), "attini/transmission.py")
                 result = rcvd_data
             except:
